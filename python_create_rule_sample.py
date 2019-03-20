e73b2405a5b5de80043202feaae820c2bbd47ee2
Kevin Doherty <kvdoherty@gmail.com>
2019-03-20 14:45:59 -0400

    Update python_create_rule_sample.py

--- python_create_rule_sample.py
+++ python_create_rule_sample.py
@@ -11,7 +11,7 @@ UN = ''
 PWD = ''
 
 # Insert Your Account Name and Stream Label Into the URL
-base_url = 'https://gnip-api.twitter.com/rules/powertrack/accounts/<INSERT_ACCOUNT_NAME_HERE>/publishers/twitter/<INSERT_STREAM_LABEL_HERE>.json'
+base_url = 'https://gnip-api.twitter.com/rules/powertrack/accounts/<INSERT_ACCOUNT_NAME_HERE>/publishers/twitter/<INSERT_STREAM_LABEL_HERE>.json?rule='
 
 # Create URL Structure
 class RequestWithMethod(urllib.request.Request):
@@ -19,25 +19,21 @@ class RequestWithMethod(urllib.request.Request):
 		self._method = method
 		urllib.request.Request.__init__(self, base_url, headers)
 
-	def get_method(self):
+	def post_method(self):
 		if self._method:
 			return self._method
 		else:
-			return urllib.request.Request.get_method(self)
+			return urllib.request.Request.post_method(self)
 
 # Create Endpoint and Add Credentials
-def create_rule_endpoint():
+def create_rule_endpoint(rule):
 	# Create sample rule with rule tag
-	rule = 'coffee'
-	tag = 'coffee'
-	values = ('{"rules": [{"value":"' + rule + '","tag":"' + tag + '"}]}').encode()
-
-	# Attach password and username to URL
 	base64string = ('%s:%s' % (UN, PWD)).replace('\n', '')
 	base = base64.b64encode(base64string.encode('ascii'))
-	final_url = urllib.request.Request(url=base_url, data=values)
+	encoded_query = urllib.parse.urlencode({'value' : rule})
+	new_url = base_url + encoded_query
+	final_url = urllib.request.Request(new_url)
 	final_url.add_header('Authorization', 'Basic %s' % base.decode('ascii'))
-	print("finalurl: %s" % final_url)
 	return final_url
 
 # Take in the Endpoint and Make the Request
@@ -45,12 +41,12 @@ def post_rule(rules_endpoint):
 	try:
 		response = urllib.request.urlopen(rules_endpoint)
 		response_data = response.read()
-		print("RESPONSE: %s" % response_data.decode('ascii'))
+		print("RESPONSE: %s" % response_data.decode('UTF-8'))
 	except urllib.request.HTTPError as e:
-		print("ERROR: %s" % e.decode('ascii'))
+		print(e)
 
 # Set the Rule Endpoint to a Variable
-post_rules_endpoint = create_rule_endpoint()
+post_rules_endpoint = create_rule_endpoint('(sports OR news OR technology) today')
 
 # Make the Request by Passing in Rule Endpoint
 post_rule(post_rules_endpoint)
